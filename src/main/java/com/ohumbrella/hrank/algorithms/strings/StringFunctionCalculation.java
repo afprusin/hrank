765c0af131d4509459119ecedfd8f3ebc9879520
Andrew Prusinowski <afprusin@gmail.com>
2018-06-16 15:04:02 -0400

    Moved logic out of substring bean; cleaned up commenting and variable names

--- src/main/java/com/ohumbrella/hrank/algorithms/strings/StringFunctionCalculation.java
+++ src/main/java/com/ohumbrella/hrank/algorithms/strings/StringFunctionCalculation.java
@@ -9,74 +9,25 @@ import java.util.stream.IntStream;
 
 public class StringFunctionCalculation {
 
-	// Representative of the last character in a traversal for a given group;
-	private class LexicalNode {
+	private class SubstringGroup {
 		List<Integer> indexes;
 		private int startingOffset;
 
-		public LexicalNode(List<Integer> indexes) {
+		SubstringGroup(List<Integer> indexes) {
 			this.indexes = indexes;
 			this.startingOffset = 0;
 		}
 
-		public LexicalNode(List<Integer> indexes, int startingOffset) {
+		SubstringGroup(List<Integer> indexes, int startingOffset) {
 			this.indexes = indexes;
 			this.startingOffset = startingOffset;
 		}
 
-		public boolean isValidAtOffset(String textSearch, int indexOffset) {
-			return areIndexesValid(textSearch.length(), indexOffset)  &&
-					areCharactersEqual(textSearch, indexOffset);
-
+		List<Integer> getIndexes() {
+			return indexes;
 		}
 
-		private boolean areIndexesValid(int textLength, int indexOffset) {
-			boolean isValid = true;
-			for(Integer index : indexes) {
-				if(index + indexOffset >= textLength) {
-					isValid = false;
-					break;
-				}
-			}
-			return isValid;
-		}
-
-		private boolean areCharactersEqual(String textSearch, int indexOffset) {
-			boolean isEqual = true;
-			char firstChar = textSearch.charAt(indexes.get(0) + indexOffset);
-			for(Integer index : indexes) {
-				if(textSearch.charAt(index + indexOffset) != firstChar) {
-					isEqual = false;
-					break;
-				}
-			}
-			return isEqual;
-		}
-
-		public List<LexicalNode> getNodeSplitAtOffset(String searchText, int indexOffset) {
-			return indexes.stream()
-					.filter(index -> index + indexOffset < searchText.length())
-					.collect(Collectors.groupingBy(index -> searchText.charAt(index + indexOffset))).values().stream()
-					.map(indexes -> new LexicalNode(indexes, indexOffset))
-					.collect(Collectors.toList());
-		}
-
-		// Determine the potential maximum score of this group, and determine if it can exceed the current best
-		// TODO: Verify this assumption that this computation isn't particularly burdensome
-		public int getMaximumPotentialGroupScore(int searchTextLength) {
-			int bestScore = 0;
-
-			for(int i = 0; i < indexes.size(); i++) {
-				final int maxLength = searchTextLength - indexes.get(i);
-				final int comboScore = (i + 1) * maxLength;
-				if(comboScore > bestScore) {
-					bestScore = comboScore;
-				}
-			}
-			return bestScore;
-		}
-
-		public int getStartingOffset() {
+		int getStartingOffset() {
 			return startingOffset;
 		}
 	}
@@ -85,31 +36,27 @@ public class StringFunctionCalculation {
 		new StringFunctionCalculation().performSolution();
 	}
 
-	//TODO: Pretty this up
 	public void performSolution() {
 		String input = getInput();
-		ArrayDeque<LexicalNode> nodes = new ArrayDeque<>(getInitialNodes(input));
+		ArrayDeque<SubstringGroup> nodes = new ArrayDeque<>(getInitialGroups(input));
 
 		int bestScore = 0;
 		while( ! nodes.isEmpty()) {
-			//System.out.println(nodes.size());
-			LexicalNode currentNode = nodes.pop();
+			SubstringGroup currentGroup = nodes.pop();
 
-			int currentOffset = currentNode.getStartingOffset();
-			//System.out.println(currentOffset);
-			while(currentNode.isValidAtOffset(input, currentOffset)) {
+			int currentOffset = currentGroup.getStartingOffset();
+			while(isGroupValidAtOffset(currentGroup, input, currentOffset)) {
 				currentOffset++;
 			}
-			final int currentScore = (currentOffset) * currentNode.indexes.size();
-			if(currentScore > bestScore) {
-				bestScore = currentScore;
+
+			final int groupScore = getGroupScore(currentGroup, currentOffset);
+			if(groupScore > bestScore) {
+				bestScore = groupScore;
 			}
 
-			//System.out.println(currentOffset);
-
-			List<LexicalNode> splitNodes = currentNode.getNodeSplitAtOffset(input, currentOffset);
-			for(LexicalNode splitNode : splitNodes) {
-				if(splitNode.getMaximumPotentialGroupScore(input.length()) > bestScore) {
+			List<SubstringGroup> splitNodes = getGroupsSeparatedByCharacterAtOffset(currentGroup, input, currentOffset);
+			for(SubstringGroup splitNode : splitNodes) {
+				if(getMaximumPotentialGroupScore(splitNode, input.length()) > bestScore) {
 					nodes.push(splitNode);
 				}
 			}
@@ -120,8 +67,7 @@ public class StringFunctionCalculation {
 
 	private String getInput() {
 		String inputLine;
-		//try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-		try(BufferedReader reader = new BufferedReader((new InputStreamReader(this.getClass().getResourceAsStream("/StringFunctionCalculation10"))))) {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
 			inputLine = reader.readLine().trim();
 		} catch (IOException e) {
 			throw new IllegalStateException(e);
@@ -130,11 +76,68 @@ public class StringFunctionCalculation {
 		return inputLine;
 	}
 
-	private List<LexicalNode> getInitialNodes(String toIndex) {
+	private List<SubstringGroup> getInitialGroups(String toIndex) {
 		return IntStream.range(0, toIndex.length() - 1)
 				.boxed()
 				.collect(Collectors.groupingBy(toIndex::charAt)).values().stream()
-				.map(LexicalNode::new)
+				.map(SubstringGroup::new)
+				.collect(Collectors.toList());
+	}
+
+	private int getGroupScore(SubstringGroup group, int substringLength) {
+		return group.getIndexes().size() * substringLength;
+	}
+
+	// The group's (or a subset group's) maximum score, assuming it could match the remaining text
+	private int getMaximumPotentialGroupScore(SubstringGroup group, int searchTextLength) {
+		final List<Integer> indexes = group.getIndexes();
+		int bestScore = 0;
+		for(int i = 0; i < indexes.size(); i++) {
+			final int maxLength = searchTextLength - indexes.get(i);
+			final int comboScore = (i + 1) * maxLength;
+			if(comboScore > bestScore) {
+				bestScore = comboScore;
+			}
+		}
+		return bestScore;
+	}
+
+	private boolean isGroupValidAtOffset(SubstringGroup group, String textSearch, int indexOffset) {
+		return areIndexesValid(group, textSearch.length(), indexOffset)  &&
+				areCharactersEqual(group, textSearch, indexOffset);
+
+	}
+
+	private boolean areIndexesValid(SubstringGroup group, int textLength, int indexOffset) {
+		boolean isValid = true;
+		for(Integer index : group.getIndexes()) {
+			if(index + indexOffset >= textLength) {
+				isValid = false;
+				break;
+			}
+		}
+		return isValid;
+	}
+
+	private boolean areCharactersEqual(SubstringGroup group, String textSearch, int indexOffset) {
+		boolean isEqual = true;
+		char firstChar = textSearch.charAt(group.getIndexes().get(0) + indexOffset);
+		for(Integer index : group.getIndexes()) {
+			if(textSearch.charAt(index + indexOffset) != firstChar) {
+				isEqual = false;
+				break;
+			}
+		}
+		return isEqual;
+	}
+
+	private List<SubstringGroup> getGroupsSeparatedByCharacterAtOffset(
+			SubstringGroup group, String searchText, int indexOffset) {
+		return group.getIndexes().stream()
+				.filter(index -> (index + indexOffset) < searchText.length())
+				.collect(Collectors.groupingBy(index ->
+						searchText.charAt(index + indexOffset))).values().stream()
+				.map(indexes -> new SubstringGroup(indexes, indexOffset))
 				.collect(Collectors.toList());
 	}
 }
