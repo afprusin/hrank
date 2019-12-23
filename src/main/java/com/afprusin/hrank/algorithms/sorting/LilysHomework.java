14a8a4559b614851ba9128edd990fa5598db2710
Andrew Fentruck Prusinowski <andrew.prusinowski@backstopsolutions.com>
2019-12-23 15:26:38 -0600

    Now checks both ascending and descending sorts
    The problem description was somewhat unclear as to what
    constituted the minimum ordering of elements.  The correct
    definition would be 'the sum of the absolute difference between
    all sequential elements'

--- src/main/java/com/afprusin/hrank/algorithms/sorting/LilysHomework.java
+++ src/main/java/com/afprusin/hrank/algorithms/sorting/LilysHomework.java
@@ -5,22 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LilysHomework {
 
-    private int performSolution() {
-        return getNeededSwaps(getInput());
+    private int getMinimumSwaps() {
+        List<Integer> unsorted = getInput();
+
+        return Math.min(
+                getNeededSwapsForSortIndexes(getSortIndexesAscending(unsorted)),
+                getNeededSwapsForSortIndexes(getSortIndexesDescending(unsorted)));
     }
 
     private List<Integer> getInput() {
         List<Integer> inputValues;
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            String metadataLine = reader.readLine();
+            final String metadataLine = reader.readLine();
             String valuesLine = reader.readLine();
 
             inputValues = Arrays.stream(valuesLine.split(" "))
@@ -33,74 +35,70 @@ public class LilysHomework {
         return inputValues;
     }
 
-    private int getNeededSwaps(List<Integer> unsorted) {
-        List<Integer> sortedIndices = IntStream.range(0, unsorted.size())
-                .boxed().sorted(Comparator.comparing(unsorted::get).reversed())
-                .collect(Collectors.toList());
 
-        boolean[] wasSwapped = new boolean[sortedIndices.size()];
+
+    private int getNeededSwapsForSortIndexes(List<Integer> sortIndexes) {
+        final boolean[] wasSwapped = new boolean[sortIndexes.size()];
 
         int swapTotal = 0;
-        for (Integer i = 0; i < wasSwapped.length; i++) {
+        for (int i = 0; i < wasSwapped.length; i++) {
             if (!wasSwapped[i]) {
-                swapTotal += doSwapChain(i, sortedIndices, wasSwapped);
+                final int loopSize = getLoopSizeAndSetSwapped(i, sortIndexes, wasSwapped);
+                swapTotal += (loopSize - 1);
             }
         }
 
         return swapTotal;
     }
 
-    private int doSwapChain(
+    private List<Integer> getSortIndexesAscending(List<Integer> unsorted) {
+        return getSortIndexes(unsorted, getAscendingComparator(unsorted));
+    }
+
+    private List<Integer> getSortIndexesDescending(List<Integer> unsorted) {
+        return getSortIndexes(unsorted, getDescendingComparator(unsorted));
+    }
+
+    private List<Integer> getSortIndexes(List<Integer> unsorted, Comparator<Integer> comparator) {
+        return IntStream.range(0, unsorted.size())
+                .boxed()
+                .sorted(comparator)
+                .collect(Collectors.toList());
+    }
+
+    private Comparator<Integer> getAscendingComparator(List<Integer> unsorted) {
+        return Comparator.comparing(unsorted::get);
+    }
+
+    private Comparator<Integer> getDescendingComparator(List<Integer> unsorted) {
+        return Comparator.comparing(unsorted::get).reversed();
+    }
+
+    private int getLoopSizeAndSetSwapped(
             final Integer startingIndex,
             final List<Integer> sortedIndexes,
             final boolean[] wasSwapped) {
-        int swaps = 0;
         wasSwapped[startingIndex] = true;
         Integer nextIndex = sortedIndexes.get(startingIndex);
 
-        System.out.print(startingIndex+","+nextIndex);
+        int loopSize = 1;
 
         if (startingIndex.equals(nextIndex)) {
-            System.out.println();
-            return 0;
+            return loopSize;
         }
         wasSwapped[nextIndex] = true;
 
-        do {
-            nextIndex = sortedIndexes.get(nextIndex);
-            System.out.print(","+nextIndex);
-
-            // DEBUG
-            if (wasSwapped[nextIndex]) {
-                System.out.println("Index was already true: " + nextIndex);
-            }
-
+        while (!nextIndex.equals(startingIndex)) {
             wasSwapped[nextIndex] = true;
-            swaps++;
-        } while (!nextIndex.equals(startingIndex));
-
-        System.out.println();
-        return swaps;
-    }
-
-    private void checkDuplicates() {
-        Set<String> dupeCheck = new HashSet<>();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            Arrays.stream(reader.readLine().split(" "))
-                    .map(item -> item.trim())
-                    .forEach(item -> {
-                        boolean isDupe = dupeCheck.add(item);
-                        if (!isDupe) {
-                            System.out.println(item + " was duplicate");
-                        }
-                    });
-        } catch (Exception e) {
+            loopSize++;
+            nextIndex = sortedIndexes.get(nextIndex);
         }
+
+        return loopSize;
     }
 
     public static void main(String[] args) {
-        System.out.println(new LilysHomework().performSolution());
-        //new LilysHomework().checkDuplicates();
+        System.out.println(new LilysHomework().getMinimumSwaps());
     }
 
 }
